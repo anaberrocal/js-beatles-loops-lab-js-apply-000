7b5806ec5b268c430d720c4a4615c211a58b188f
Ana Berrocal <anaberrocal@ymail.com>
2020-02-12 20:46:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ function theBeatlesPlay() {
   const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
   const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-for(let i = 0; i < 6; i ++)
-  for(let i in instruments; i < instruments.length; i ++)
+for(let i = 0; i < 5; i ++)
+  for(let i = 0; i < 5; i ++)
 
     return (`${musicians[i]} plays ${instruments[i]}`)
 
